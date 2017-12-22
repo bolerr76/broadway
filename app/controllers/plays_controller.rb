350eb95883229c3d528d208d97767a7059b58194
bole <zvrkolyna@yahoo.com>
2017-12-22 13:01:09 +0000

    Change Category filtering on Index Page and Index Action

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -3,7 +3,12 @@ class PlaysController < ApplicationController
     
     
     def index
+      if params[:category].blank?
         @plays = Play.all.order("created_at DESC")
+      else
+        @category_id = Category.find_by(name: params[:category]).id
+        @plays = Play.where(:category_id => @category_id).order("created_at DESC")
+      end
     end
     
     def show
@@ -52,11 +57,7 @@ class PlaysController < ApplicationController
         params.require(:play).permit(:title, :description, :director, :category_id)
     end
     
-    private
-    
     def find_play
          @play = Play.find(params[:id])
     end
-    
-    
 end
